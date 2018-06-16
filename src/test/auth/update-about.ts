f2b1cf635e7ca457654115217a468be8a0511325
jeswin <jeswinpk@agilehead.com>
2018-06-16 17:50:04 +0530

    Fix the auth-about test

--- src/test/auth/update-about.ts
+++ src/test/auth/update-about.ts
@@ -12,19 +12,22 @@ export default function(msgSource: IMessageSource) {
     const reply1 = await handle(command1, message1, msgSource);
 
     const command2 =
-      "id jeswin about Your life is your life. Don't let it be clubbed into dank submission.";
+      "id jeswin about. Your life is your life. Don't let it be clubbed into dank submission.";
     const message2 = createMessage({ text: command2 });
     const reply2 = await handle(command2, message2, msgSource);
 
     shouldLib.exist(reply2);
     const _ =
-      reply2 && reply2.message.should.equal("Updated profile information for jeswin.");
+      reply2 &&
+      reply2.message.should.equal("Updated profile information for jeswin.");
 
     {
       const db = await getDb();
       const rows = db.prepare(`SELECT * FROM identity WHERE id="jeswin"`).all();
-      shouldLib.exist(rows[0]);
-      rows[0].enabled.should.equal(1);
+      rows.length.should.equal(1);
+      rows[0].about.should.equal(
+        "Your life is your life. Don't let it be clubbed into dank submission."
+      );
     }
   };
 }
